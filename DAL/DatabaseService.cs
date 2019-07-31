753002b92685db024226b55f746f08995aa0d6fd
ngo nui <ngoducnui6789@gmail.com>
2019-07-31 22:45:12 +0700

    Xóa readdata

--- DAL/DatabaseService.cs
+++ DAL/DatabaseService.cs
@@ -49,16 +49,6 @@ namespace DAL
             SqlDataReader reader = command.ExecuteReader();
             return reader;
         }
-        public SqlDataReader ReadData(string sql)
-        {
-            command = new SqlCommand();
-            command.CommandType = CommandType.Text;
-            command.CommandText = sql;
-            command.Connection = connection;
-            OpenConnection();
-            SqlDataReader reader = command.ExecuteReader();
-            return reader;
-        }
         public bool WriteData(string sql, SqlParameter[] parameters)
         {
             command = new SqlCommand();
@@ -66,7 +56,7 @@ namespace DAL
             command.CommandText = sql;
             command.Connection = connection;
             OpenConnection();
-            command.Parameters.AddRange(pars);
+            command.Parameters.AddRange(parameters);
             int kq = command.ExecuteNonQuery();
             return kq > 0;
         }
